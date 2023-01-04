39ec9160dba324c535cd68b09bdbbf295311c42e
Ayodeji Ikujuni <eniolayodeji@gmail.com>
2023-01-04 17:41:49 +0100

    added register

---
+++ src/pages/register.jsx
@@ -0,0 +1,119 @@
+import {
+  Box,
+  Heading,
+  HStack,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Icon,
+  Text,
+  VStack,
+  Button,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineEye, AiOutlineMail } from "react-icons/ai";
+
+import { BiLockAlt } from "react-icons/bi";
+import { FiFacebook, FiTwitter } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <VStack my="20" spacing={"5"}>
+      <Box
+        bg="white"
+        w={{ base: "80vw", xl: "772px" }}
+        py="40px"
+        borderRadius={"16"}
+      >
+        <HStack justify={"center"} spacing="7">
+          <Heading fontSize={"18px"} fontWeight="700" color={"secondary.grey"}>
+            Sign in
+          </Heading>
+          <Heading
+            fontSize={"22px"}
+            fontWeight="700"
+            color={"primary.dark"}
+            position="relative"
+            zIndex={"3"}
+            _before={{
+              content: `""`,
+              width: "100%",
+              height: 2,
+              backgroundColor: "primary.pink",
+              position: "absolute",
+              zIndex: "-1",
+              bottom: 1,
+            }}
+          >
+            Register
+          </Heading>
+        </HStack>
+        <form
+          style={{
+            width: "374px",
+            margin: "0 auto",
+          }}
+        >
+          <InputGroup borderColor="secondary.grey" mt="60px">
+            <InputLeftElement pointerEvents="none" py="6">
+              <Icon as={AiOutlineMail} boxSize={"26"} color="primary.dark" />
+            </InputLeftElement>
+            <Input
+              type="text"
+              placeholder="Email"
+              color="secondary.grey"
+              borderRadius={"16px"}
+              opacity={1}
+              py="6"
+              _placeholder={{ opacity: 1, color: "secondary.grey" }}
+            />
+          </InputGroup>
+          <InputGroup borderColor="secondary.grey" mt="4" mb="2">
+            <InputLeftElement pointerEvents="none" py="6">
+              <Icon as={BiLockAlt} boxSize={"26"} color="primary.dark" />
+            </InputLeftElement>
+            <Input
+              type="text"
+              placeholder="Password"
+              color="secondary.grey"
+              borderRadius={"16px"}
+              opacity={1}
+              py="6"
+              _placeholder={{ opacity: 1, color: "secondary.grey" }}
+            />
+            <InputRightElement pointerEvents="none" py="6">
+              <Icon as={AiOutlineEye} boxSize={"26"} color="primary.dark" />
+            </InputRightElement>
+          </InputGroup>
+
+          <Link
+            to="/"
+            style={{
+              textAlign: "right",
+            }}
+          >
+            <Text color={"primary.dark"} fontSize="12" fontWeight={"600"}>
+              Forgot password?
+            </Text>
+          </Link>
+          <Button
+            w="full"
+            borderRadius="16px"
+            bg="primary.green"
+            _hover={{ bg: "primary.green" }}
+            py="10"
+            mt="60px"
+          >
+            Sign In
+          </Button>
+        </form>
+      </Box>
+    </VStack>
+  );
+}
+
+export default Register;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import AuthWrapper from "../components/AuthWrapper";
 import SignIn from "../pages/signIn";
+import Register from "../pages/register";
 import Livestream from "../pages/livestream";
 
 function LayoutRoute() {
@@ -9,7 +10,7 @@ function LayoutRoute() {
       <Route path="/" exact element={<Livestream />} />
       <Route element={<AuthWrapper />}>
         <Route path="/sign-in" element={<SignIn />} />
-        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   );
